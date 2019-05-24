ea609e63e9087d1cb4dae9d432d031fbd24cb9e8
Nikita <pogodin_nikitka@bk.ru>
2019-05-25 00:54:49 +0300

    25.05.2019

--- src/js/parts/modal-visit.js
+++ src/js/parts/modal-visit.js
@@ -1,27 +1,21 @@
 function modalVisit() {
 
-    let delayPopup = 3000;
+    let delayPopup = 60000;
 
     function show() {
-        let popup = document.querySelector('.popup'),
-            close = document.querySelector('.popup_close');
+        let popup = document.querySelector('.popup');
             
         popup.style.display = 'block';
         document.body.style.overflow = 'hidden';
 
-        // При клике на крестик форма становится невидимой
-        close.addEventListener('click', () => {
-            popup.style.display = 'none';             
-            document.body.style.overflow = '';
-        });
-
-        // При клике на подложку форма становится невидимой
+        // При клике на подложку или крестик форма становится невидимой
         popup.addEventListener('click', (event) => {
 
             let target = event.target,
-                content = document.querySelector('.popup_content');
+                content = document.querySelector('.popup_content'),
+                close = document.querySelector('.popup_close');
                 
-            if (!content.contains(target)) {
+            if (!content.contains(target) || close.contains(target)) {
                 popup.style.display = 'none';
                 document.body.style.overflow = '';
             } else {
