a879c0e27de0dc9b3e07b6d2d01623bea92f208d
Nikita <pogodin_nikitka@bk.ru>
2019-05-25 00:03:47 +0300

    25.05.2019

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -185,6 +185,32 @@ module.exports = modalPopup;
 
 function modalVisit() {
 
+    let delayPopup = 60000;
+
+    function show() {
+        let popup = document.querySelector('.popup'),
+            close = document.querySelector('.popup_close');
+            
+        popup.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+
+        // При клике на крестик форма становится невидимой
+        close.addEventListener('click', () => {
+            popup.style.display = 'none';             
+            document.body.style.overflow = '';
+        });
+
+        // При клике на подложку форма становится невидимой
+        popup.addEventListener('click', (event) => {
+            let target = event.target,
+                content = document.querySelector('.popup_content');
+                
+            if (!content.contains(target)) popup.style.display = 'none';
+
+        });
+    }
+    
+    setTimeout(show, delayPopup);
 }
 
 module.exports = modalVisit;
